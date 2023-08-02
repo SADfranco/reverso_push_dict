afa0bd58f84011c12d4c5e49ff86d616c30fb273
Andrei Shabaev <shabaevandrew@gmail.com>
2023-08-02 18:38:33 +0300

    rm env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-TOKEN = '6166691144:AAFu7ozWXPF1w_iQ7zPsxoaKYXix7P_0jQU'  # заменить на токен своего бота
-USER = "shabaevandrew@gmail.com"
-PASSWORD = "Aw253634"
-CHAT_ID = '507969801'
-
-TIMEZONE = 'Europe/Berlin'
-TIMEZONE_COMMON_NAME = 'Berlin'
-
